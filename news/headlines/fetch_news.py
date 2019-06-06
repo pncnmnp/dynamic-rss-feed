96d7a88e72592cfd4ff831c46d407cf4f4aee8da
parthparikh <parthparikh1999p@gmail.com>
2019-06-06 23:20:22 +0530

    updated fetch_news method

--- news/headlines/fetch_news.py
+++ news/headlines/fetch_news.py
@@ -1,12 +1,42 @@
 import feedparser
+import pickle
 from json import load
+from datetime import datetime
+from os import path
 
 class fetch_news:
 	def __init__(self):
 		self.news_content = {}
+		self.directory = './datasets/past_news/'
+		self.file = str()
+
+	def get_filename(self):
+		self.file = datetime.now().strftime("%d-%m-%Y") + '.pickle'
+
+	def check_news(self):
+		'''
+            Checks if the news is older than 2 hours or 
+                   if the news' file does not exists
+                   if above is True, scrape news.
+                   else return False
+		'''
+		self.get_filename()
+		if path.isfile(self.directory + self.file) == True:
+			curr_hour = datetime.now().hour
+			past_hour = datetime.fromtimestamp(
+							path.getmtime(self.directory + self.file)
+						).hour
+			if curr_hour - past_hour > 2:
+				self.scrape()
+				return True
+			return False
+
+		else:
+			self.scrape()
+			return True
 
 	def scrape(self):
-		url_link_path = './datasets/categories.json'
+		url_link_path = './datasets/news_urls.json'
 		urls = load(open(url_link_path))
 
 		for category in urls:
@@ -28,6 +58,14 @@ class fetch_news:
 						index += 1
 					except:
 						break
+
+		# store scraped news in pickle format
+		self.pickle_news()
+
+	def pickle_news(self):
+		dbfile = open(self.directory + self.file, 'wb')
+		pickle.dump(self.news_content, dbfile)
+		dbfile.close()
 
 if __name__ == '__main__':
 	obj = fetch_news()
