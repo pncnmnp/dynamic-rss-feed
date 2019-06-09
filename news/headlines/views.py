eb371d8c31e535734ddcb2f34bcd196926750187
parthparikh <parthparikh1999p@gmail.com>
2019-06-09 11:52:00 +0530

    minor changes: added support to remove duplicate and outdated news

--- news/headlines/views.py
+++ news/headlines/views.py
@@ -10,7 +10,7 @@ from dateutil import parser
 
 def store(request):
 	past_time = datetime.now() - timedelta(seconds=1)
-	past_day = datetime.now() - timedelta(days=1)
+	past_day = datetime.now() - timedelta(hours=6)
 	curr_timestamp = datetime.fromtimestamp(path.getmtime('./db.sqlite3'))
 
 	if past_day > curr_timestamp:
@@ -35,12 +35,20 @@ def store(request):
 			while(True):
 				try:
 					datetime_obj = parser.parse(news_content[category][index]['date'])
+
+					# Checks if the news year is same as current year
+					# This removes outdated news
+					if datetime_obj.year < datetime.now().year:
+						index += 1
+						continue
 					news_instance, created = Title.objects.get_or_create(
 						title_text=news_content[category][index]['title'],
 						pub_date=datetime_obj,
-						news_url=news_content[category][index]['links'],
-						description=news_content[category][index]['desc'],
-						news_category=category
+						defaults={
+							'news_url': news_content[category][index]['links'],
+							'description': news_content[category][index]['desc'],
+							'news_category': category
+						}
 					)
 					if created == True:
 						news_instance.save()
