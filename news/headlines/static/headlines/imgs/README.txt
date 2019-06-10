4d9a06b425e5ed7f52552316f8c0ebfb0ed570b0
parthparikh <parthparikh1999p@gmail.com>
2019-06-10 09:17:43 +0530

    syndication feed is connected with templates in headlines app.

---
+++ news/headlines/static/headlines/imgs/README.txt
@@ -0,0 +1,2 @@
+The RSS icons are under CC 3.0 BY. See http://creativecommons.org/licenses/by/3.0/ for the complete licensing details.
+These icons are taken from Flaticon. For more see: https://www.flaticon.com/free-icon/social-rss-circle-internet_10010
--- news/headlines/urls.py
+++ news/headlines/urls.py
@@ -12,12 +12,12 @@ from headlines.rss_feeds.business import BusinessFeed
 urlpatterns = [
 	path('', views.fetch, name='all_news'),
 	path('update/', views.store, name='get_headlines'),
-	path('feeds/top/', TopFeed()),
-	path('feeds/world/', WorldFeed()),
-	path('feeds/tech/', TechFeed()),
-	path('feeds/sports/', SportsFeed()),
-	path('feeds/science/', ScienceFeed()),
-	path('feeds/india/', IndiaFeed()),
-	path('feeds/cricket/', CricketFeed()),
-	path('feeds/business/', BusinessFeed())
+	path('feeds/top/', TopFeed(), name='feed_top'),
+	path('feeds/world/', WorldFeed(), name='feed_world'),
+	path('feeds/tech/', TechFeed(), name='feed_tech'),
+	path('feeds/sports/', SportsFeed(), name='feed_sports'),
+	path('feeds/science/', ScienceFeed(), name='feed_science'),
+	path('feeds/india/', IndiaFeed(), name='feed_india'),
+	path('feeds/cricket/', CricketFeed(), name='feed_cricket'),
+	path('feeds/business/', BusinessFeed(), name='feed_business')
 ]
